eee87e1b3a981dc34c2f769371597f00cce070d3
Samisahariar <samisahariar69@gmail.com>
2024-11-07 07:39:48 -0800

    this is the commit of the day

--- problem-1.ts
+++ problem-1.ts
@@ -1,3 +1,9 @@
-{
-    
+
+type SumofNumbers = (params: number[]) => void;
+const sumArray: SumofNumbers = (params) => {
+    let sum: number = 0
+    params.forEach((element, idx) => {
+        sum += params[idx]
+    })
+    console.log(sum)
 }
---
+++ problem-7.ts
@@ -0,0 +1,19 @@
+{
+    class Car {
+        make: string
+        model: string
+        year: number
+        constructor(make: string, model: string, year: number) {
+            this.make = make
+            this.model = model
+            this.year = year
+        }
+
+        getCarAge() {
+            console.log(2024 - this.year)
+        }
+
+    }
+
+    
+}
